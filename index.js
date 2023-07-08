8a28d49f5b9cdead0bd6723f3b2df563ded48ef6
adityahonal <adityahongalwork@.com>
2023-07-08 20:32:16 +0530

    Encapsulation

---
+++ index.js
@@ -0,0 +1,23 @@
+// ENCAPSULATION TUTORIAL 
+
+// Procedural method (before OOPS concept this was used and it created lot of complications)
+// let baseSalary = 30_000;                            //properties seperate
+// let overTime = 10;
+// let rate = 20;
+//
+// function getWage(baseSalary, overTime, rate) {      //functions seperate with 3 parameters
+//    return baseSalary + (overTime * rate)
+// };
+
+// ENCAPSULATION METHOD 
+// let employee = {                      ---------> This is object
+//    baseSalary : 30_000;                  ---------> These are poperties
+//    overtime : 10;
+//    rate : 20
+//
+//    getWage : function(){                 ---------> This is method 
+//                                                        (No parameters)
+//        return this.baseSalary + (this.overtime * this.rate);
+//  };
+// }
+// employee.getWage();
