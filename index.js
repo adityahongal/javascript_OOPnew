19bd77c45bc2c5f1979cf8448ceb6847092e9d20
adityahonal <adityahongalwork@.com>
2023-07-09 21:25:51 +0530

    object literal syntax

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-// ENCAPSULATION TUTORIAL 
+// Object literal tutorial
 
-// Procedural method (before OOPS concept this was used and it created lot of complications)
-// let baseSalary = 30_000;                            //properties seperate
-// let overTime = 10;
-// let rate = 20;
-//
-// function getWage(baseSalary, overTime, rate) {      //functions seperate with 3 parameters
-//    return baseSalary + (overTime * rate)
+// const circle = {};  circle is an object & {}; curly braces is object literal syntax
+
+// const circle = {
+//    radius:1,                                   //-------> properties
+//    location : {
+//        x:1,
+//        y:1
+//    },
+//    draw: function(){                          //-------> Method/function
+//        console.log('draw');
+//    }
 // };
-
-// ENCAPSULATION METHOD 
-// let employee = {                      ---------> This is object
-//    baseSalary : 30_000;                  ---------> These are poperties
-//    overtime : 10;
-//    rate : 20
-//
-//    getWage : function(){                 ---------> This is method 
-//                                                        (No parameters)
-//        return this.baseSalary + (this.overtime * this.rate);
-//  };
-// }
-// employee.getWage();
-//
-
-// Abstraction tutorial
+// circle.draw();                                 //-------> We can access members using dot notation
